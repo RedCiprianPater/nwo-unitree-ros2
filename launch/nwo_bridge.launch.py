f83994f2af7a5efec2e4f9907c9a5dc84c02da25
Ciprian Pater <ciprianpater@gmail.com>
2026-03-28 02:45:57 +0100

    Add files via upload

--- launch/nwo_bridge.launch.py
+++ launch/nwo_bridge.launch.py
@@ -1,1 +1,15 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
 
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='nwo_unitree_bridge',
+            executable='api_bridge_node',
+            name='nwo_unitree_bridge',
+            output='screen',
+            parameters=[
+                {'use_sim_time': False}
+            ]
+        )
+    ])
